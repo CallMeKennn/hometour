585e8b1236c3a935607b578774f1fdbd40292b39
Nguyễn Huy Hoàng <101138030+CallMeKennn@users.noreply.github.com>
2024-01-18 20:23:51 +0700

    Home Page

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Categories.scss";
 
-const Categories = () => {
-    return <div>Categories</div>;
+const Categories = ({ src, categoryName }) => {
+    return (
+        <div className="category-container">
+            <img className="img-category" src={src} />
+            <div className="category-name">{categoryName}</div>
+        </div>
+    );
 };
 
 export default Categories;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,24 +1,44 @@
 import React from "react";
 import Navbar2 from "../../Components/Navbar2/Navbar2";
 import "./Home.scss";
-import Moai1 from "../../Assets/img/moai_1.png"
-import Moai2 from "../../Assets/img/moai_2.png"
-import YellowMoai from "../../Assets/img/yellow_moai.png"
+import Categories from "../../Components/Categories/Categories";
+import Moai1 from "../../Assets/img/moai_1.png";
+import Moai2 from "../../Assets/img/moai_2.png";
+import YellowMoai from "../../Assets/img/yellow_moai.png";
+import FriendMoai from "../../Assets/img/Moai_Va_Nhung_Nguoi_Ban.png";
+import BroMoai from "../../Assets/img/Anh_Em_Moai.png";
 
 const Home = () => {
     return (
         <div className="home-container">
             <Navbar2 />
             <div className="header-home">
-                <div>
+                <div className="slogan">
                     <h1>Furniture that everyone loves</h1>
-                    <div>We have 5000+ reivew and our customers trust on our furniture and quality products</div>
-                    <button>Buy now</button>
-                    <button>Explone</button>
+                    <div className="review">
+                        We have 5000+ reivew and our customers trust on our furniture and quality products
+                    </div>
+                    <div className="">
+                        <button className="button buy-now">Buy now</button>
+                        <button className="button explone">Explone</button>
+                    </div>
                 </div>
-                <img src={Moai1} alt=""/>
-                <img src={Moai2} alt=""/>
-                <img src={YellowMoai} alt=""/>
+                <img className="img-position moai2" src={Moai2} alt="" />
+                <img className="img-position moai1" src={Moai1} alt="" />
+                <img className="img-position yellowMoai" src={YellowMoai} alt="" />
+                {/* <img className="img-position yellowMoai" src={BroMoai} alt="" /> */}
+            </div>
+
+            <div className="categories-container">
+                <div>
+                    <h2>Browse The Range</h2>
+                    <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+                </div>
+                <div className="categories">
+                    <Categories src={BroMoai} categoryName={"Hoang"}/>
+                    <Categories src={BroMoai} categoryName={"Hoang"}/>
+                    <Categories src={BroMoai} categoryName={"Hoang"}/>
+                </div>
             </div>
         </div>
     );
